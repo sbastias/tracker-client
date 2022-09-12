49de353e3c11245df6255d5c725fa48e130bf0ec
Sergio Bastias <sergio@dishproductions.com>
2022-09-11 23:02:28 -0400

    Sticky controls, multiple tbody elements (PlacemntRow components) revised design for sticky element, toggle row onclick, maxmin on Filters AND Columns, resize on UI Change

--- components/tracker/overlays/update-placement.vue
+++ components/tracker/overlays/update-placement.vue
@@ -8,7 +8,7 @@
     <h4>{{placement.AVTRRT__Job_Title__c}}</h4>
     <h5>{{placement.AVTRRT__Employer__r.Name}} ({{placement.Supplier__c}})</h5>
     <h5>{{placement.AVTRRT__Contact_Candidate__r.FirstName}} {{placement.AVTRRT__Contact_Candidate__r.LastName}}</h5>
-    <h5>${{placement.AVTRRT__Pay_Rate__c}} <span v-if="placement.AVTRRT__Contact_Candidate__r.Pay_Rate_Adjustment__c">(+{{placement.AVTRRT__Contact_Candidate__r.Pay_Rate_Adjustment__c}})</span></h5>
+    <h5 v-if="placement.AVTRRT__Pay_Rate__c">${{placement.AVTRRT__Pay_Rate__c}} <span v-if="placement.AVTRRT__Contact_Candidate__r.Pay_Rate_Adjustment__c">(+{{placement.AVTRRT__Contact_Candidate__r.Pay_Rate_Adjustment__c}})</span></h5>
 
 <!--
 
